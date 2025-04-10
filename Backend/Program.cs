71124d96cc1b56d4e004f84925cc3b122b2080d5
Sabi_Varga <varg.sab@gmail.com>
2025-04-10 11:13:38 +0200

    Update Kestrel Port

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
-    serverOptions.ListenAnyIP(80);
+    serverOptions.ListenAnyIP(8080);
 });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
